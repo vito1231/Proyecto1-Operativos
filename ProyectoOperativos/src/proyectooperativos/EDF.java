e54d47383d67bb09ddaf350e0d33c6faa4570f4a
Victor Navarro <vito1231@github.com>
2019-10-01 11:25:29 -0600

    commit obligado

--- ProyectoOperativos/src/proyectooperativos/EDF.java
+++ ProyectoOperativos/src/proyectooperativos/EDF.java
@@ -5,11 +5,57 @@
  */
 package proyectooperativos;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author vicnb
  */
 public class EDF {
     private int len;
+    private String[] processLine;
+    private ArrayList<Process> procesos;
+     
+
+    public EDF(int len, ArrayList<Process> pro) {
+        this.len = len;
+        processLine=new String[len];
+        procesos=pro;
+        
+    }
+
+    public EDF() {
+    }
+
+    public void apply(){
+        
+    }
+    
+    
+    public int getLen() {
+        return len;
+    }
+
+    public void setLen(int len) {
+        this.len = len;
+    }
+
+    public String[] getProcessLine() {
+        return processLine;
+    }
+
+    public void setProcessLine(String[] processLine) {
+        this.processLine = processLine;
+    }
+
+    public ArrayList<Process> getProcesos() {
+        return procesos;
+    }
+
+    public void setProcesos(ArrayList<Process> procesos) {
+        this.procesos = procesos;
+    }
+    
+    
     
 }
